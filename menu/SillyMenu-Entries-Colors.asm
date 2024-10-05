ff99b7bb6f8944c39a815275a450d07ceb25b47a
Peter Dell <peter.dell@web.de>
2024-10-05 11:56:30 +0200

    Update entries text and colors

--- menu/SillyMenu-Entries-Colors.asm
+++ menu/SillyMenu-Entries-Colors.asm
@@ -8,7 +8,7 @@
 ;	Chromas specified the PAL color values with luma zero.
 
 ;              LO, LO, XL, PO, GT, GF, 25, 16, DE, G2, GA, WI, TI.
-lengths	.byte  15, 10 , 10, 11, 10, 12,  9,  7, 13, 11, 10, 16, 11,0
-chromas	.byte $30,$10,$e0,$d0,$b0,$90,$70,$50,$40,$20,$f0,$c0,$00,$00
+lengths	.byte  18, 11 , 5,  7, 11, 10,  9,  7, 10, 10, 10, 11, 31, 2, 0
+chromas	.byte $30,$10,$e0,$d0,$b0,$90,$70,$50,$40,$20,$f0,$c0,$a0,$00
 ;chroma .byte $30,$10,$e0,$d0,$b0,$90,$70,$50,$40,$20,$f0,$c0,$a0,$00
 
