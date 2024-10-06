599878bc4c13a46e1b6a2ba216d14f0c969b38c5
Peter Dell <peter.dell@web.de>
2024-10-06 05:40:16 +0200

    Complete pack

--- menu/SillyMenu-Texts.asm
+++ menu/SillyMenu-Texts.asm
@@ -4,5 +4,5 @@
 ;	@com.wudsn.ide.asm.mainsourcefile=..\asm\SillyMenu.asm
 
 	.macro m_credits_text
-	.byte 'Disk compilation and menu by JAC! on 2024-09-??. ASCII by sim. Music by Alex.'
+	.byte 'Disk compilation and menu by JAC! on 2024-10-06. ASCII by sim. Music by Alex.'
 	.endm
